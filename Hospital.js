d9d286cf5ab4f3d2b67ba90d343793a174b0039a
joanpardogine <joanpardo@ginebro.cat>
2023-02-10 17:28:07 +0100

    Pujats els fitxers de classes

---
+++ Hospital.js
@@ -0,0 +1,25 @@
+'use strict'
+
+class Hospital {
+    constructor(nom, maximPacients, maximMetges) {
+        this.nomHospital = nom;
+        this.maximPacients = maximPacients;
+        this.pacientsIngressats = [];
+        this.maximMetges = maximMetges;
+    }
+
+    ingressarPacient(pacient) {
+        if (pacient != null && (this.pacientsIngressats.length < this.maximPacients))
+            this.pacientsIngressats.push(pacient);
+    }
+
+    donarDaltaPacient(llitPacient) {
+        alert("S'ha donat d'alta el pacient " + this.pacientsIngressats[llitPacient].nom + " de la malaltia " + llistaMalalties[this.pacientsIngressats[llitPacient].malaltia]);
+        this.pacientsIngressats[llitPacient] = {};
+    }
+
+    morirPacient(llitPacient) {
+        alert("Encara que s'ha fet tot el que s'ha pogut, el pacient " + this.pacientsIngressats[llitPacient].nom + " ha mort de la malaltia " + llistaMalalties[this.pacientsIngressats[llitPacient].malaltia]);
+        this.pacientsIngressats[llitPacient] = {};
+    }
+}
---
+++ Pacient.js
@@ -0,0 +1,8 @@
+'use strict'
+
+class Pacient extends Persona {
+  constructor(nomRebut, cognomRebut, nifRebut, malaltiaRebuda) {
+    super(nomRebut, cognomRebut, nifRebut);
+    this.malaltia = malaltiaRebuda;
+  }
+}
